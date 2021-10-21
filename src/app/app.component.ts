08fcd1b6bef4e3d2009e2986e5128f8591c71f8d
Keniyz <31661562+Keniyz@users.noreply.github.com>
2021-10-21 11:41:07 +0300

    update eslint
    update eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app--Uroot',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'TEST';
+
+  ngOnInit() {
+    const qwer = 'qwq';
+    qwer = 'qazxc';
+  }
 }
