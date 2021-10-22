9a1226fe26efd97137292fec6e2a659940c14e34
Keniyz <31661562+Keniyz@users.noreply.github.com>
2021-10-22 17:04:34 +0300

    Lab2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,72 @@
-import {Component, OnInit} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app--Uroot',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'TEST';
-
-  ngOnInit() {
-    const qwer = 'qwq';
-    qwer = 'qazxc';
+export class AppComponent {
+  //const a: Int;
+  public computerChoice:any = 0;
+  public result:any=0;
+  public result_of_game:string='';
+  onCLICK(data:number) {
+    this.computerChoice = Math.random()
+    // this.computerChoice < 0.40 = камінь
+    // this.computerChoice < 0.70 = бумага
+    // this.computerChoice < 1 = ножиці
+    this.result_of_game='';
+    this.result = data;
+    this.comparePARAM(this.computerChoice,  this.result)
   }
+
+  comparePARAM(comp: any, user: any){
+    //
+    if (comp === user) { this.result_of_game='Нічія!'}
+    //Якщо користувач вибрав бумагу
+    else if (user === 1) {
+        //Компютер вибрав камінь
+        if (comp > 0 && comp <= 0.40) {
+          this.result_of_game='Ви перемогли!!'
+        }
+        //Компютер вибрав бумагу
+        else if (comp > 0.40 && comp <= 0.70) {
+          this.result_of_game='Нічія!'
+       }
+        //Компютер вибрав ножиці
+        else if(comp > 0.70 && comp <= 1) {
+          this.result_of_game='Ви програли!'
+        }
+    }
+    //Якщо користувач вибрав ножиці
+     else if (user === 2) {
+        //Компютер вибрав камінь
+        if (comp > 0 && comp <= 0.40) {
+          this.result_of_game='Ви програли!'
+        }
+        //Компютер вибрав бумагу
+        else if (comp > 0.40 && comp <= 0.70) {
+          this.result_of_game='Ви перемогли!!'
+        }
+        //Компютер вибрав ножиці
+        else if(comp > 0.70 && comp <= 1) {
+          this.result_of_game='Нічія!'
+        }
+     }
+    //Якщо користувач вибрав камінь
+    else if (user === 3) {
+      //Компютер вибрав камінь
+      if (comp > 0 && comp <= 0.40) {
+        this.result_of_game='Нічія!'
+      }
+      //Компютер вибрав бумагу
+      else if (comp > 0.40 && comp <= 0.70) {
+        this.result_of_game='Ви перемогли!!'
+      }
+      //Компютер вибрав ножиці
+      else if(comp > 0.70 && comp <= 1) {
+        this.result_of_game='Ви перемогли!!'
+      }
+    }
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Lesson1Component } from './feature/lesson1/lesson1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Lesson1Component
   ],
   imports: [
     BrowserModule
